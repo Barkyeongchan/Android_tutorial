b5e4359e1688192fca60ca9e561a6de5ffaac4f0
ãPark <xodudcksxo@gmail.com>
2025-09-15 15:10:49 +0900

    RadioButton 생성

--- app/src/main/java/com/cookandroid/helloandroid/MainActivity.java
+++ app/src/main/java/com/cookandroid/helloandroid/MainActivity.java
@@ -1,67 +1,13 @@
 package com.cookandroid.helloandroid;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.Color;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
 public class MainActivity extends AppCompatActivity {
-
-    Button button1;
-    Button button2;
-    Button button3;
-    Button button4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1 = (Button) findViewById(R.id.button1);
-        button1.setBackgroundColor(Color.GRAY);
-        button1.setTextColor(Color.BLACK);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://google.com"));
-                startActivity(intent);
-            }
-        });
-
-        button2 = (Button) findViewById(R.id.button2);
-        button2.setBackgroundColor(Color.GREEN);
-        button2.setTextColor(Color.BLACK);
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:/911"));
-                startActivity(intent);
-            }
-        });
-
-        button3 = (Button) findViewById(R.id.button3);
-        button3.setBackgroundColor(Color.RED);
-        button3.setTextColor(Color.BLACK);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("content://media/internal/images/media"));
-                startActivity(intent);
-            }
-        });
-
-        button4 = (Button) findViewById(R.id.button4);
-        button4.setBackgroundColor(Color.YELLOW);
-        button4.setTextColor(Color.BLACK);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
     }
 }
